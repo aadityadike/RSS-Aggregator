d4c09dc9b3c442c415002c7cca43aec4e797a0e8
aadityadike <adityadike123@gmail.com>
2023-07-24 08:44:34 +0530

    Project Intialized

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello")
+	godotenv.Load()
+	// get port
+	portString := os.Getenv("PORT")
+	// check port is there or not.
+	if portString == "" {
+		log.Fatal("PORT is not found in environment")
+	}
+
+	fmt.Println("PORT", portString)
+	// new Route
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server is starting on %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal((err))
+	}
 }
