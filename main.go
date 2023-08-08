c98fed5ec26b0902eee5d8fab7c903536b827375
aadityadike <adityadike123@gmail.com>
2023-08-08 19:16:17 +0530

    database creation & making post request for database

--- main.go
+++ main.go
@@ -1,72 +1,95 @@
-// Importing required packages
 package main
 
 import (
+	// standard libraries
+	"database/sql"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
-
-	// Importing the "chi" router library
+	// non-standard libraries
+	"github.com/aadityadike/RSS-Aggregator/internal/database"
 	"github.com/go-chi/chi"
-	// Importing the "cors" middleware for handling CORS (Cross-Origin Resource Sharing)
 	"github.com/go-chi/cors"
-	// Importing the "godotenv" package to load environment variables from a .env file
 	"github.com/joho/godotenv"
 )
 
+// apiConfig stores the configuration for the API.
+type apiConfig struct {
+	DB *database.Queries // Instance of the database queries.
+}
+
 func main() {
-	// Loading environment variables from .env file (if present)
+	// Load environment variables from .env file (if present).
 	godotenv.Load()
 
-	// Get the port number from the environment variables
+	// Get the port number from the environment variables.
 	portString := os.Getenv("PORT")
 
-	// Check if the PORT is defined or not. If not, the server cannot start, so we log a fatal error and exit the program.
 	if portString == "" {
 		log.Fatal("PORT is not found in environment")
 	}
 
-	// Create a new router instance using "chi"
-	router := chi.NewRouter()
+	// Get the database connection URL from the environment variables.
+	dbUrl := os.Getenv("DB_URL")
 
-	// Configuration for Cross-Origin Resource Sharing (CORS)
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},                   // Allow requests from any HTTP or HTTPS origin
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Allow specific HTTP methods
-		AllowedHeaders:   []string{"*"},                                       // Allow any headers in the request
-		ExposedHeaders:   []string{"Link"},                                    // Expose specific headers in the response
-		AllowCredentials: false,                                               // Do not allow sending credentials (e.g., cookies) with the requests
-		MaxAge:           300,                                                 // Cache preflight request results for 5 minutes
-	}))
-
-	// Create a new sub-router for version 1 of the API
-	v1Router := chi.NewRouter()
-
-	// Define a route for "/v1/healthz" and associate it with the "handlerReadiness" function
-	v1Router.Get("/healthz", handlerReadiness)
-	// Define a route for "/v1/err" and associate it with the "handlerError" function
-	v1Router.Get("/err", handlerError)
-
-	// Mount the v1Router under the "/v1" prefix in the main router
-	router.Mount("/v1", v1Router)
-
-	// Create an HTTP server instance
-	srv := &http.Server{
-		Handler: router,           // Set the router as the handler for the server
-		Addr:    ":" + portString, // Set the address to listen on with the provided port
+	if dbUrl == "" {
+		log.Fatal("DB_URL is not found in environment")
 	}
 
-	// Log a message indicating that the server is starting on the specified port
+	// Open a connection to the PostgreSQL database.
+	connection, err := sql.Open("postgres", dbUrl)
+
+	if err != nil {
+		log.Fatal("Unable to connect to the database", err)
+	}
+
+	// Create a new instance of the API configuration.
+	apiconf := apiConfig{
+		DB: database.New(connection),
+	}
+
+	// Create a new router instance using "chi".
+	router := chi.NewRouter()
+
+	// Configure Cross-Origin Resource Sharing (CORS) settings.
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},                   // Allow requests from any HTTP or HTTPS origin.
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Allow specific HTTP methods.
+		AllowedHeaders:   []string{"*"},                                       // Allow any headers in the request.
+		ExposedHeaders:   []string{"Link"},                                    // Expose specific headers in the response.
+		AllowCredentials: false,                                               // Do not allow sending credentials (e.g., cookies) with requests.
+		MaxAge:           300,                                                 // Cache preflight request results for 5 minutes.
+	}))
+
+	// Create a sub-router for version 1 of the API.
+	v1Router := chi.NewRouter()
+
+	// Define routes and associate them with handler functions.
+	v1Router.Get("/healthz", handlerReadiness)         // Route for checking API readiness.
+	v1Router.Get("/err", handlerError)                 // Route for triggering an error.
+	v1Router.Post("/users", apiconf.handlerCreateUser) // Route for creating users.
+
+	// Mount the v1Router under the "/v1" prefix in the main router.
+	router.Mount("/v1", v1Router)
+
+	// Create an HTTP server instance.
+	srv := &http.Server{
+		Handler: router,           // Set the router as the handler for the server.
+		Addr:    ":" + portString, // Set the address to listen on with the provided port.
+	}
+
+	// Log a message indicating that the server is starting on the specified port.
 	log.Printf("Server is starting on %v", portString)
 
-	// Start listening and serving HTTP requests
-	err := srv.ListenAndServe()
+	// Start listening and serving HTTP requests.
+	err = srv.ListenAndServe()
 
 	/*
-	 * This will return err after the server stops listening. The point is that our server will run forever,
-	 * but if an error occurs during listening (e.g., the port is already in use), it will be caught by the following if statement.
+	 * If an error occurs during listening (e.g., the port is already in use),
+	 * it will be caught by the following if statement.
 	 */
 	if err != nil {
-		log.Fatal((err))
+		log.Fatal(err)
 	}
 }
