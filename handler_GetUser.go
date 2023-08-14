88b76e4ac6b32e4c52a03afbcc4a082361b71571
aadityadike <adityadike123@gmail.com>
2023-08-14 18:57:45 +0530

    getting user by ApiKey

--- handler_GetUser.go
+++ handler_GetUser.go
@@ -1,33 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/aadityadike/RSS-Aggregator/internal/auth"
 )
 
 func (config *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		ApiKey string `json:"apiKey"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-
-	params := parameters{}
-
-	err := decoder.Decode(&params)
+	apikey, err := auth.GetApiKey(r.Header)
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON %v", err))
-		return
+		respondWithError(w, 403, fmt.Sprintf("Error while getting an ApiKey %v", err))
 	}
 
-	user, err := config.DB.GetUserByAPIKey(r.Context(), params.ApiKey)
+	user, err := config.DB.GetUserByAPIKey(r.Context(), apikey)
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error creating user %v", err))
-		return
+		respondWithError(w, 400, fmt.Sprintf("Error while getting user %v", err))
 	}
 
-	respondWithJson(w, 200, databaseUserToUser(user))
+	respondWithJson(w, 201, databaseUserToUser(user))
 }
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -13,10 +13,19 @@ func GetApiKey(headers http.Header) (string, error) {
 	value := headers.Get("Authorization")
 
 	if value == "" {
-		return "", errors.New("authorization error occurs while getting Api key")
+		return "", errors.New("authentication error occurs while getting Api key")
 	}
 
 	val := strings.Split(value, " ")
 
-	
+	if len(val) != 2 {
+		return "", errors.New("authentication values are not correct")
+	}
+
+	if val[0] != "ApiKey" {
+		return "", errors.New("header is not correct please specify correct headers")
+	}
+
+	return val[1], nil
+
 }
